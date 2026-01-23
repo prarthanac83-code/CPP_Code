c6bf972da30a89023e87512f89a96e3d1bb017b8
prarthanac83-code <prarthanac83@gmail.com>
2026-01-23 13:56:18 +0000

    23/1

--- cprogram/do.c
+++ cprogram/do.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char password="" 
-}
+    char password[]="Hello";
+    char input[20];
+do{
+    printf("Enter the password!");
+    scanf("%s",input);
+}while(strcmp(input,password)!=0);
+    printf("access granted!");
+}    
+
