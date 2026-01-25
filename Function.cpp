b35756e624cb2ed16ae4475fd443388dae136524
prarthanac83-code <prarthanac83@gmail.com>
2026-01-25 13:41:28 +0000

    25/1

---
+++ Function.cpp
@@ -0,0 +1,11 @@
+//simple function program
+//addition of two numbers
+#include<iostream>
+using namespace std;
+int add(int x,int y){
+return x+y;
+}
+int main(){
+    int sum=add(7,5);
+    cout<<"Addition="<<sum<<endl;
+}
