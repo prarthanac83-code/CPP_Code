991a904f93283d37c3ab47784287a636e1559f73
prarthanac83-code <prarthanac83@gmail.com>
2026-02-04 13:05:02 +0000

    4/2

---
+++ Java /Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse{
+    public static void main(String args[]){
+        int age = 20;
+        if(age>=18){
+        System.out.println("You are eligible for voting");
+    }else{
+        System.out.println("You are not eligible for voting");
+        
+    }
+}
+}
---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php
+$age=19;
+if($age>=18)
+    echo "n/Eligibe for voting!n/
+";
+else 
+    echo "Not eligible for voting!"; 
+?>
