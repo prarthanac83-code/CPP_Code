0cc99681c7b42547d7d303a68363456e573d0937
prarthanac83-code <prarthanac83@gmail.com>
2026-04-06 13:10:05 +0000

    6/4

---
+++ Java /ThreadExp.java
@@ -0,0 +1,13 @@
+class MyThread extends Thread{
+    public void run(){
+        for(int i=1;i<=5;i++){
+        System.out.println("Thread running:"+i);
+    }
+  }
+}
+public class ThreadExp {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start(); 
+    }
+}
