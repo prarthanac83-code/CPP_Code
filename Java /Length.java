04f0ee689a56aa8bd22d381cd638256567b9c00a
prarthanac83-code <prarthanac83@gmail.com>
2026-04-11 14:38:39 +0000

    11/4

---
+++ Java /Length.java
@@ -0,0 +1,6 @@
+public class Length {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Length of string:"+str.length());
+    }
+}
---
+++ Java /Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        String s1 ="Welcome";
+        String s2 ="Java";
+        System.out.println(s1+" "+s2);
+    }
+}
