eebeaf8315b3497749d451935190c72081df9d09
prarthanac83-code <prarthanac83@gmail.com>
2026-08-19 12:18:07 +0000

    19/8

---
+++ S.P/A1.java
@@ -0,0 +1,33 @@
+//1 Hello World 
+// public class A1{
+// public static void main(String args[]){
+// System.out.println("Hello World!");
+// }
+// }
+
+// 2 Addition 
+// public class A1{
+//     public static void main(String args[]){
+//         int a=10;
+//         int b=10;
+//         System.out.println("Addtion of two numbers is " + (a+b));
+//     }
+// }
+
+// 3 Substraction
+// public class A1{
+//     public static void main(String args[]){
+//         int a = 20;
+//         int b = 10;
+//         System.out.println("Substraction of two numbers is " + (a-b));
+//     }
+// }
+
+// 4 Multiplication
+// public class A1{
+//     public static void main(String args[]){
+//     int a = 5;
+//     int b = 2;
+//     System.out.println("Multiplication of two numbers is " + (a*b));
+//     }
+// }
